e179095d46dce7a6b7a14073a16d19ee92bf6dcb
alif farhan <aliffarhanromadhani@gmail.com>
2026-06-28 21:50:51 +0700

    Update arc interaction

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
-setTimeout(()=>{
+setTimeout(() => {
+
+    document
+        .getElementById("loader")
+        .classList
+        .add("hidden")
+
+}, 2500)
+
+
+
 document
-.getElementById("loader")
-.classList
-.add("hidden")
-},2500)
-document
-.querySelectorAll(
-".box,.role,.social-card"
-)
-.forEach(el=>{
-el.addEventListener(
-"mousemove",
-e=>{
-const r=
-el.getBoundingClientRect()
-el.style.transform=`rotateX(
-${(e.clientY-r.top-100)/18}deg
-)
-rotateY(
-${-(e.clientX-r.left-100)/18}deg)`})
-el.addEventListener(
-"mouseleave",
-()=>{
-el.style.transform=""
-})})
+
+    .querySelectorAll(
+
+        ".box,.role,.social-card"
+
+    )
+
+    .forEach(card => {
+
+        card.addEventListener(
+
+            "mousemove",
+
+            e => {
+
+                const r =
+                    card.getBoundingClientRect()
+
+                const x =
+                    (e.clientX - r.left - r.width / 2) / 18
+
+                const y =
+                    (e.clientY - r.top - r.height / 2) / 18
+
+                card.style.transform =
+                    `
+rotateX(${-y}deg)
+rotateY(${x}deg)
+`
+
+            }
+
+        )
+
+        card.addEventListener(
+
+            "mouseleave",
+
+            () => {
+
+                card.style.transform = ""
+
+            }
+
+        )
+
+    })
